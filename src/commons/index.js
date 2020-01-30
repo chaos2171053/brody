232522813ab579a514b66104d62a991a9eb77580
chaos2171053 <chaos2171053@gmail.com>
2020-01-30 16:49:12 +0800

    build(precommit,husky,commitlint): add

--- src/commons/index.js
+++ src/commons/index.js
@@ -1,9 +1,12 @@
-import {session} from '@/library/utils/storage';
-import {getNodeByPropertyAndValue, convertToTree} from '@/library/utils/tree-utils';
-import pathToRegexp from 'path-to-regexp/index';
-import {ROUTE_BASE_NAME} from '@/router/AppRouter';
+import { session } from "@/library/utils/storage";
+import {
+    getNodeByPropertyAndValue,
+    convertToTree
+} from "@/library/utils/tree-utils";
+import pathToRegexp from "path-to-regexp/index";
+import { ROUTE_BASE_NAME } from "@/router/AppRouter";
 
-const CURRENT_USER_KEY = 'current-user';
+const CURRENT_USER_KEY = "current-user";
 
 const localStorage = window.localStorage;
 const sessionStorage = window.sessionStorage;
@@ -22,13 +25,13 @@ export function hasPermission(code) {
  */
 export function setLoginUser(currentUser = {}) {
     // 将用户属性在这里展开，方便查看系统都用到了那些用户属性
-    const {id, name, avatar, token, permissions} = currentUser;
+    const { id, name, avatar, token, permissions } = currentUser;
     const userStr = JSON.stringify({
-        id,             // 用户id 必须
-        name,           // 用户名 必须
-        avatar,         // 用头像 非必须
-        token,          // 登录凭证 非必须 ajax请求有可能会用到，也许是cookie
-        permissions,    // 用户权限
+        id, // 用户id 必须
+        name, // 用户名 必须
+        avatar, // 用头像 非必须
+        token, // 登录凭证 非必须 ajax请求有可能会用到，也许是cookie
+        permissions // 用户权限
     });
 
     sessionStorage.setItem(CURRENT_USER_KEY, userStr);
@@ -56,7 +59,7 @@ export function isLogin() {
  */
 export function toHome() {
     // 跳转页面，优先跳转上次登出页面
-    const lastHref = window.sessionStorage.getItem('last-href');
+    const lastHref = window.sessionStorage.getItem("last-href");
 
     // 强制跳转 进入系统之后，需要一些初始化工作，需要所有的js重新加载
     window.location.href = lastHref || `${ROUTE_BASE_NAME}/`;
@@ -66,7 +69,7 @@ export function toHome() {
  * 跳转到登录页面
  */
 export function toLogin() {
-    const loginPath = '/login';
+    const loginPath = "/login";
 
     // 判断当前页面是否已经是login页面，如果是，直接返回，不进行跳转，防止出现跳转死循环
     const pathname = window.location.pathname;
@@ -78,7 +81,7 @@ export function toLogin() {
     session.clear();
     localStorage.setItem(CURRENT_USER_KEY, null);
     sessionStorage.clear();
-    sessionStorage.setItem('last-href', window.location.pathname);
+    sessionStorage.setItem("last-href", window.location.pathname);
 
     // 强制跳转，让浏览器刷新，重置数据
     window.location.href = `${ROUTE_BASE_NAME}${loginPath}`;
@@ -93,33 +96,42 @@ export function toLogin() {
  * @returns {*}
  */
 export function getSelectedMenuByPath(path, menuTreeData) {
-    path = path.replace(ROUTE_BASE_NAME, '');
+    path = path.replace(ROUTE_BASE_NAME, "");
     let selectedMenu;
     if (menuTreeData) {
-        if (path.indexOf('/_') > -1) {
-            path = path.substring(0, path.indexOf('/_'));
+        if (path.indexOf("/_") > -1) {
+            path = path.substring(0, path.indexOf("/_"));
         }
 
         // 先精确匹配
-        selectedMenu = getNodeByPropertyAndValue(menuTreeData, 'path', path, (itemValue, value, item) => {
-            const isTop = item.children && item.children.length;
-            return itemValue === value && !isTop; // 排除父级节点
-        });
+        selectedMenu = getNodeByPropertyAndValue(
+            menuTreeData,
+            "path",
+            path,
+            (itemValue, value, item) => {
+                const isTop = item.children && item.children.length;
+                return itemValue === value && !isTop; // 排除父级节点
+            }
+        );
 
         // 正则匹配，路由中有`:id`的情况
         // fixme 容易出问题：a/b/:id,会匹配 a/b/1, a/b/detail，有可能不是期望的结果，注意路由写法
         // fixme: a/b/tab/:id 具体的:id，添加一级，用来表明id是什么
-        if (!selectedMenu && path !== '/') {
-            selectedMenu = getNodeByPropertyAndValue(menuTreeData, 'path', path, (itemValue, value, item) => {
-                const isTop = item.children && item.children.length;
-                const re = pathToRegexp(itemValue);
-                return !!re.exec(value) && !isTop; // 排除父级节点
-            });
+        if (!selectedMenu && path !== "/") {
+            selectedMenu = getNodeByPropertyAndValue(
+                menuTreeData,
+                "path",
+                path,
+                (itemValue, value, item) => {
+                    const isTop = item.children && item.children.length;
+                    const re = pathToRegexp(itemValue);
+                    return !!re.exec(value) && !isTop; // 排除父级节点
+                }
+            );
         }
     }
     return selectedMenu;
 }
-
 
 /**
  * 获取菜单树状结构数据 和 随菜单携带过来的权限
@@ -128,13 +140,13 @@ export function getSelectedMenuByPath(path, menuTreeData) {
 export function getMenuTreeDataAndPermissions(menus) {
     // 用户权限code，通过菜单携带过来的 1 => 菜单 2 => 功能
     const permissions = menus.map(item => {
-        if (item.type === '1') return item.key;
-        if (item.type === '2') return item.code;
+        if (item.type === "1") return item.key;
+        if (item.type === "2") return item.code;
         return null;
     });
 
     // 获取菜单，过滤掉功能码
-    menus = menus.filter(item => item.type !== '2');
+    menus = menus.filter(item => item.type !== "2");
 
     // 处理path： 只声明了url，为iframe页面
     menus = menus.map(item => {
@@ -158,9 +170,11 @@ export function getMenuTreeDataAndPermissions(menus) {
     });
 
     // 设置顶级节点path，有的顶级没有指定path，默认设置为子孙节点的第一个path
-    const findPath = (node) => {
-        const children = orderedData.filter(item => item.parentKey === node.key);
-        let path = '';
+    const findPath = node => {
+        const children = orderedData.filter(
+            item => item.parentKey === node.key
+        );
+        let path = "";
         if (children && children.length) {
             for (let i = 0; i < children.length; i++) {
                 const child = children[i];
@@ -181,6 +195,5 @@ export function getMenuTreeDataAndPermissions(menus) {
     });
 
     const menuTreeData = convertToTree(orderedData);
-    return {menuTreeData, permissions}
+    return { menuTreeData, permissions };
 }
-
