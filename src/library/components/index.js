af3a1022bedd76eb87c7eee89bdc5b5ac082a315
chaos2171053 <chaos2171053@gmail.com>
2020-01-31 17:36:38 +0800

    build(typescript): support typescript

--- src/library/components/index.js
+++ src/library/components/index.js
@@ -1,19 +1,42 @@
-export AsyncSelect from './async-select';
-export ColorPicker  from './color-picker';
-export ContextMenu  from './context-menu';
-export DataBlock  from './data-block';
-export DraggableTabsBar from './draggable-tabs-bar';
-export FormElement from './form-element';
-export FormRow from './form-row';
-export IconPicker from './icon-picker';
-export JsonEditor  from './json-editor';
-export modal  from './modal-hoc';
-export ModalContent from './modal-hoc/ModalContent';
-export Operator  from './operator';
-export Pagination from './pagination';
-export QueryBar  from './query-bar';
-export Table  from './table';
-export tableEditable  from './table-editable';
-export tableRowDraggable  from './table-row-draggable';
-export ToolBar, {ToolItem}  from './tool-bar';
-export UserAvatar  from './user-avatar';
+import AsyncSelect from "./async-select";
+import ColorPicker from "./color-picker";
+import ContextMenu from "./context-menu";
+import DataBlock from "./data-block";
+import DraggableTabsBar from "./draggable-tabs-bar";
+import FormElement from "./form-element";
+import FormRow from "./form-row";
+import IconPicker from "./icon-picker";
+import JsonEditor from "./json-editor";
+import modal from "./modal-hoc";
+import ModalContent from "./modal-hoc/ModalContent";
+import Operator from "./operator";
+import Pagination from "./pagination";
+import QueryBar from "./query-bar";
+import Table from "./table";
+import tableEditable from "./table-editable";
+import tableRowDraggable from "./table-row-draggable";
+import ToolBar, { ToolItem } from "./tool-bar";
+import UserAvatar from "./user-avatar";
+
+export {
+    AsyncSelect,
+    ColorPicker,
+    ContextMenu,
+    DataBlock,
+    DraggableTabsBar,
+    FormElement,
+    FormRow,
+    IconPicker,
+    JsonEditor,
+    modal,
+    ModalContent,
+    Operator,
+    Pagination,
+    QueryBar,
+    Table,
+    tableEditable,
+    tableRowDraggable,
+    ToolBar,
+    ToolItem,
+    UserAvatar
+};
