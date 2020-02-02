5d54f4faa64bfca74699a83910a72cc6f5c2b24d
chaos2171053 <chaos2171053@gmail.com>
2020-02-02 16:40:50 +0800

    style(color.less): remove

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import * as storage from '@/library/utils/storage';
-import * as sxRedux from '@/library/redux';
-import App from './App';
-import handleSuccess from './commons/handle-success';
-import handleError from './commons/handle-error';
-import {configureStore} from './models';
-import * as serviceWorker from './serviceWorker';
-import {getLoginUser} from './commons';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import * as storage from "@/library/utils/storage";
+import * as sxRedux from "@/library/redux";
+import App from "./App";
+import handleSuccess from "./commons/handle-success";
+import handleError from "./commons/handle-error";
+import { configureStore } from "./models";
+import * as serviceWorker from "./serviceWorker";
+import { getLoginUser } from "./commons";
+import "./index.css";
 
 // dev 模式开启mock
-if (process.env.NODE_ENV === 'development' || process.env.MOCK === 'true') {
-    require('./mock/index');
-    console.log('current mode is development, mock is enabled');
+if (process.env.NODE_ENV === "development" || process.env.MOCK === "true") {
+    require("./mock/index");
+    console.log("current mode is development, mock is enabled");
 }
 
 const currentUser = getLoginUser() || {};
 
 // 存储初始化 区分不同用户存储的数据
 storage.init({
-    keyPrefix: currentUser.id,
+    keyPrefix: currentUser.id
 });
 
-sxRedux.init({storage, handleError, handleSuccess});
+sxRedux.init({ storage, handleError, handleSuccess });
 
 // models store
 const store = configureStore();
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
